fd44e197f08fd3f55bd80f7172de24c85067f780
Shaunak Warty <shaunak.warty@gmail.com>
2023-07-24 00:29:40 -0700

    Switch to vite

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,6 @@ import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
 const theme = createTheme({
@@ -11,10 +10,7 @@ const theme = createTheme({
   },
 });
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -22,8 +18,3 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
